51d9ebd2413ffde1363325e95444eb3b03a00c07
Abhishekkrjha30 <abhishekkrjha30@gmail.com>
2026-04-02 11:37:01 +0530

    fix cros error

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ app = FastAPI()
 # ================== CORS (FIXED) ==================
 origins = [
     "https://student-ai-dashboard-frontend.vercel.app",
+    "https://student-ai-dashboard-frontend-1fwk2bvvt-abhis-projects-66a94381.vercel.app",
     "http://localhost:3000"
 ]
 
