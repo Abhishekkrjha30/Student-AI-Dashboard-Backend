54c76b8750d05994b86a81373941d55ddfd2395e
Abhishekkrjha30 <abhishekkrjha30@gmail.com>
2026-04-02 11:47:41 +0530

    fix cros error

--- main.py
+++ main.py
@@ -18,9 +18,6 @@ origins = [
     "https://student-ai-dashboard-frontend.vercel.app",
     "https://student-ai-dashboard-frontend-git-main-abhis-projects-66a94381.vercel.app",
     "https://student-ai-dashboard-frontend-1fwk2bvvt-abhis-projects-66a94381.vercel.app",
-    "https://student-ai-dashboard-frontend.vercel.app/",
-    "https://student-ai-dashboard-frontend-git-main-abhis-projects-66a94381.vercel.app/",
-    "https://student-ai-dashboard-frontend-1fwk2bvvt-abhis-projects-66a94381.vercel.app/",
     "http://localhost:3000"
 ]
 
@@ -28,7 +25,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],   # ✅ allow ALL methods (important)
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
