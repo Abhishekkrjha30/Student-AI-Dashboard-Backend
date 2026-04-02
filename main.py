abdfa7d8e7c800d77b458917c5073f8309428dc3
Abhishekkrjha30 <abhishekkrjha30@gmail.com>
2026-04-02 12:28:33 +0530

    fix cros error

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app.add_middleware(
         "https://student-ai-dashboard-frontend-1fwk2bvvt-abhis-projects-66a94381.vercel.app",
         "http://localhost:3000"
     ],
-    allow_origins=["*"]
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
